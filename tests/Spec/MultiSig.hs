d6bcd0e661cc61ca0b0fa8a008afc629f6ba4358
Alasdair Hill <alasdair.hill@iohk.io>
2024-11-22 08:48:53 +0000

    initial contract model completed

--- tests/Spec/MultiSig.hs
+++ tests/Spec/MultiSig.hs
@@ -124,6 +124,8 @@ import qualified Cardano.Node.Emulator as E
 
 type Wallet = Integer
 
+data Phase = Initial | Holding | Proposing | Limbo | DeadlinePassed | Cancelled deriving (Eq, Show, Generic)
+
 walletPubKeyHash :: Wallet -> Ledger.PubKeyHash
 walletPubKeyHash =
     Ledger.pubKeyHash
@@ -139,6 +141,9 @@ w2 = 2
 w3 = 3
 w4 = 4
 w5 = 5
+
+testWallets :: [Wallet]
+testWallets = [w1, w2, w3, w4, w5]
 
 walletAddress :: Wallet -> Ledger.CardanoAddress
 walletAddress = (E.knownAddresses !!) . pred . fromIntegral
@@ -163,15 +168,17 @@ modelParams = MultiSigParams
   , minimumValue = Ada.lovelaceValueOf 1000
   }
 
+-- TODO: Make contract get minNumSigs automatically
 data MultiSigModel = MultiSigModel
   { _possible_signatories :: [Wallet],
     _signatures :: [Wallet],
-    _minNumSignatures :: Integer,
+    _minNumSigs :: Integer,
     _minimumValue :: Integer,
     _target :: Wallet,
     _deadline :: Slot,
     _proposedValue :: Integer,
-    _contractValue :: Integer
+    _contractValue :: Integer,
+    _phase :: Phase
   }
   deriving (Eq, Show, Generic)
 
@@ -198,12 +205,13 @@ instance ContractModel MultiSigModel where
     MultiSigModel
       { _possible_signatories = [],
         _signatures = [],
-        _minNumSignatures = 0,
+        _minNumSigs = 2,
         _minimumValue = 2,
         _target = 0,
         _deadline = 0,
         _proposedValue = 0,
-        _contractValue = 0
+        _contractValue = 0,
+        _phase = Initial
       }
 
   nextState a = void $ case a of
@@ -211,16 +219,17 @@ instance ContractModel MultiSigModel where
       target .= t
       proposedValue .= v
       deadline .= s
-      wait 2
+      phase .= Holding
+      wait 1
     AddSig w -> do
       signatures %= (w :)
       wait 1
 {-    Donate w v -> do
       contractValue %= (+ v)
       withdraw (walletAddress w) (Ada.adaValueOf $ fromInteger v) -}
-      wait 1
     Cancel _w -> do
       signatures .= []
+      phase .= Cancelled
       wait 1
     Pay _w -> do
       oldContractValue <- viewContractState contractValue
@@ -228,23 +237,60 @@ instance ContractModel MultiSigModel where
       proposedValue <- viewContractState proposedValue
       deposit (walletAddress target) (Ada.adaValueOf $ fromInteger proposedValue)
       contractValue .= (oldContractValue - proposedValue)
+      signatures .= []
+      phase .= Proposing
       wait 1
     Open w v -> do
       contractValue %= (+ v)
       withdraw (walletAddress w) (Ada.adaValueOf $ fromInteger v)
+      phase .= Proposing
       wait 1
 
 -- To implement to really use Quickcheck dynamic and contract model
 -- It defines when an action can be triggered or not
 -- No worries, we'll also try to trigger then if you said it's not possible, sometimes, 👀
-  precondition _s a = case a of
-    Propose _v _w _t _s -> True
-    AddSig _w -> True
- --   Donate w v -> True
-    Cancel _w -> True
-    Pay _w -> True
-    Open _w _v -> True
-   
+  precondition s a = case a of
+    Propose v _w _t _s ->
+      (currentPhase == Proposing || currentPhase == Cancelled)
+      && v < contractVal
+    AddSig _w -> currentPhase == Holding
+    Cancel _w -> currentPhase == DeadlinePassed
+    Pay w -> currentPhase == Holding
+              && (length sigs) >= (fromInteger minSigs)
+              && w /= targ -- TODO: Check this in offchain code
+    Open _w _v ->  currentPhase == Initial
+    where currentPhase = s ^. contractState . phase
+          sigs = s ^. contractState . signatures
+          minSigs = s ^. contractState . minNumSigs
+          targ = s ^. contractState . target
+          contractVal = s ^. contractState . contractValue
+
+
+  nextReactiveState slot = do
+    deadline <- viewContractState deadline
+    s <- viewContractState phase
+    when (s == Limbo) $ do
+      phase .= DeadlinePassed
+    when ((fromCardanoSlotNo slot) >= deadline && (s == Holding)) $ do
+      phase .= Limbo
+
+  arbitraryAction s =
+    frequency
+      [ (1 , Propose <$> choose @Integer (10, 30)
+                    <*> QC.elements testWallets
+                    <*> QC.elements testWallets
+                    <*> QC.elements [(slot + 10), (slot + 2)])
+      , (1 , AddSig <$> QC.elements testWallets)
+      , (1 , Cancel <$> QC.elements testWallets)
+      , (1 , Pay <$> QC.elements testWallets)
+      , (1 , Open <$> QC.elements testWallets <*> choose @Integer (50, 60))
+      ]
+    where
+      slot = s ^. currentSlot . to fromCardanoSlotNo
+      {- beforeRefund = slot < s ^. contractState . refundSlot
+      afterRefund = Prelude.not beforeRefund
+      prefer b = if b then 10 else 1 -}
+
   validFailingAction _ _ = False
 
 instance RunModel MultiSigModel E.EmulatorM where
@@ -301,8 +347,47 @@ unitTest1 = do
   action $ AddSig w5
   action $ Pay w4
 
+unitTest2 :: DL MultiSigModel ()
+unitTest2 = do
+  action $ Open w3 51
+  waitUntilDL 7
+  action $ Propose 10 w1 w2 9
+  action $ Cancel 2
+  action $ Propose 10 w1 w2 12
+
+
+-- Bug in offchain
+unitTest3 :: DL MultiSigModel ()
+unitTest3 = do
+  action $ Open w3 57
+  action $ Propose 30 w1 w5 12
+  action $ AddSig w4
+  action $ AddSig w5
+  action $ Pay w5
+
+unitTest4 :: DL MultiSigModel ()
+unitTest4 = do
+  action $ Open w2 54
+  action $ Propose 26 w1 w5 13
+  action $ AddSig w3
+  action $ AddSig w1
+  action $ Pay w2
+  action $ Propose 29 w2 w5 13
+
+
 prop_Check :: QC.Property
-prop_Check = QC.withMaxSuccess 1 $ QC.noShrinking $ forAllDL unitTest1 prop_MultiSig
+prop_Check = QC.withMaxSuccess 1 $ QC.noShrinking $ forAllDL unitTest4 prop_MultiSig
+
+
+{-
+ [[+]Open 5 59,
+  [+]Propose 28 4 3 (Slot {getSlot = 127}),
+  [+]AddSig 4,
+  [+]AddSig 1,
+  [+]Pay 5,
+  [+]Propose 16 1 4 (Slot {getSlot = 137}),
+  [+]Pay 3]
+-}
 
 
 tests :: TestTree
