a73c2c4c7f875559692b500374d268732a02d313
Bogdan Manole <bogdan.manole@frago.ro>
2024-11-26 14:47:20 +0200

    chore: adding the coverage at the end

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,19 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Main(main) where
+
+module Main (main) where
 
 import Spec.Escrow qualified
 
-import Test.Tasty
+import Cardano.Node.Emulator qualified as E
+import Control.Exception
+import Data.IORef (IORef, newIORef, readIORef)
 import PlutusTx.Coverage
-import Data.IORef (IORef, newIORef)
+import Prettyprinter qualified as Pretty
+import System.Exit
+import Test.Tasty
+import Test.Tasty.Ingredients.Basic
 
 main :: IO ()
 main = do
   ref <- newIORef mempty
   defaultMain (tests ref)
+    `catch` ( \(e :: ExitCode) -> do
+                -- print coverage report
+                covData <- readIORef ref
+                let opts = Spec.Escrow.optionsCoverage ref
+                    report = CoverageReport (E.coverageIndex opts) covData
+                print $ Pretty.pretty report
+                throwIO e
+            )
 
 tests :: IORef CoverageData -> TestTree
 tests ref =
-  testGroup "use cases" [
-    Spec.Escrow.tests ref
+  testGroup
+    "use cases"
+    [ Spec.Escrow.tests ref
     ]
